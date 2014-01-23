517477cecba9b95cce40932d5a0173927958182d
Stephen James <llama@interactivellama.com>
2014-01-23 16:19:51 -0500

    Added jquery ui to bower install and main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,12 +2,13 @@ requirejs.config({
     baseUrl: 'scripts/',
     paths: {
         jquery: '/bower_components/jquery/jquery',
+        'jquery-ui': '/bower_components/jquery-ui/ui/jquery-ui',
 	    underscore: '/bower_components/underscore/underscore',
 	    backbone: '/bower_components/backbone/backbone',
     }
 });
 
-require(['require','jquery', 'underscore', 'backbone'], function(require) {
+require(['require','jquery', 'jquery-ui', 'underscore', 'backbone', 'app'], function(require) {
 	console.log('Yeah, it\'s an app!');
 
 });
